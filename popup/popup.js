3a9489c42bce1cab6da018903fcc4557e14ece50
Archit Shukla <asharchit03@gmail.com>
2024-11-15 21:00:26 +0530

    initialising repo

---
+++ popup/popup.js
@@ -0,0 +1,7 @@
+document.getElementById("toPage1").addEventListener("click", () => {
+  window.location.href = "page1.html";
+});
+
+document.getElementById("toPage2").addEventListener("click", () => {
+  window.location.href = "page2.html";
+});
